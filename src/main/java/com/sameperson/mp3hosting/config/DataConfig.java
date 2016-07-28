f18605a7c8c65cc88f3168fadbd05fa028ebad63
Saem <ottosigma@gmail.com>
2016-07-28 04:47:41 +0300

    rearranging

---
+++ src/main/java/com/sameperson/mp3hosting/config/DataConfig.java
@@ -0,0 +1,7 @@
+package com.sameperson.mp3hosting.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataConfig {
+}
---
+++ src/main/java/com/sameperson/mp3hosting/config/TemplateConfig.java
@@ -0,0 +1,35 @@
+package com.sameperson.mp3hosting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+@Configuration
+public class TemplateConfig {
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCacheable(false);
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        final ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+}
