e0d456847750c378faf74c46e13579f0bfdc4e98
Saem <ottosigma@gmail.com>
2016-08-05 15:39:44 +0300

    Java configuration for Hibernate instead of XML config

--- src/main/java/com/sameperson/mp3hosting/config/DataConfig.java
+++ src/main/java/com/sameperson/mp3hosting/config/DataConfig.java
@@ -7,12 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.servlet.MultipartConfigElement;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @PropertySource("application.properties")
@@ -22,9 +21,8 @@ public class DataConfig {
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
-        Resource config = new ClassPathResource("hibernate.cfg.xml");
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setConfigLocation(config);
+        sessionFactory.setHibernateProperties(getHibernateProperties());
         sessionFactory.setPackagesToScan(env.getProperty("mp3hosting.entity.package"));
         sessionFactory.setDataSource(dataSource());
         return sessionFactory;
@@ -46,5 +44,15 @@ public class DataConfig {
         ds.setUsername(env.getProperty("mp3hosting.db.username"));
         ds.setPassword(env.getProperty("mp3hosting.db.password"));
         return ds;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.cache.provider_class", env.getProperty("hibernate.cache.provider_class"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.implicit_naming_strategy", env.getProperty("hibernate.implicit_naming_strategy"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        return properties;
     }
 }
