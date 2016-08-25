cade098ee1425adb0830a762223508434b65d313
Saem <ottosigma@gmail.com>
2016-08-25 19:34:17 +0300

    Now lists uploaded mp3 files on the page

--- src/main/java/com/sameperson/mp3hosting/web/controller/Mp3Controller.java
+++ src/main/java/com/sameperson/mp3hosting/web/controller/Mp3Controller.java
@@ -6,10 +6,7 @@ import com.sameperson.mp3hosting.web.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,7 +21,13 @@ public class Mp3Controller {
     public String listMp3s(Model model) {
         List<Mp3> mp3s = mp3Service.findAll();
         model.addAttribute("mp3s", mp3s);
-        return "home";
+        return "mp3/index";
+    }
+
+    @RequestMapping("/mp3s/{mp3Id}.mp3")
+    @ResponseBody
+    public byte[] mp3(@PathVariable Long mp3Id) {
+        return mp3Service.findOne(mp3Id).getData();
     }
 
     @RequestMapping("/audio/{audioId}")
@@ -49,7 +52,8 @@ public class Mp3Controller {
     public String addGif(Mp3 mp3, @RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
         mp3Service.save(mp3, file);
         redirectAttributes.addFlashAttribute("flash",new FlashMessage("Mp3 successfully uploaded", FlashMessage.Status.SUCCESS));
-        return String.format("redirect:/mp3s/%s", mp3.getId());
+        return "redirect:/";
+//        return String.format("redirect:/mp3s/%s", mp3.getId());
     }
 
 }
