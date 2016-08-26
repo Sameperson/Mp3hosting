9efc121559490f4da4c8adf899af078c60ada7fc
Saem <ottosigma@gmail.com>
2016-08-26 21:25:34 +0300

    Database setup XML-file for the test, test setup fro Mp3Dao

---
+++ src/test/java/com/sameperson/mp3hosting/dao/Mp3DaoTest.java
@@ -0,0 +1,35 @@
+package com.sameperson.mp3hosting.dao;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.sameperson.mp3hosting.Application;
+import com.sameperson.mp3hosting.model.User;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@DatabaseSetup("classpath:mp3s.xml")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DbUnitTestExecutionListener.class
+})
+public class Mp3DaoTest {
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private Mp3Dao mp3Dao;
+
+    @Before
+    public void setup() {
+        User user = new User();
+        user.setId(1L);
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null));
+    }
+}
