8161dff447c7b5d70e9123b74f1b1c57c478438d
Saem <ottosigma@gmail.com>
2016-10-15 12:48:47 +0300

    Started adding sign-up functionality

--- src/main/java/com/sameperson/mp3hosting/service/UserService.java
+++ src/main/java/com/sameperson/mp3hosting/service/UserService.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface UserService extends UserDetailsService{
     User findByUsername(String username);
+    void save(User user);
 }
--- src/main/java/com/sameperson/mp3hosting/web/controller/LoginController.java
+++ src/main/java/com/sameperson/mp3hosting/web/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,6 +15,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LoginController {
     @Autowired
     private UserService userService;
+
+    @RequestMapping("/sign-up")
+    public String signUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return null;
+    }
+
+    @RequestMapping(value = "/sign-up", method = RequestMethod.POST)
+    public String signUp(User user, RedirectAttributes redirectAttributes) {
+        userService.save(user);
+        return "redirect:/";
+    }
 
     @RequestMapping("/login")
     public String login(Model model,  HttpServletRequest request) {
